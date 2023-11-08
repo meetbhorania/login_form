4af02b3d6340544784a9fb0ce69c292ee4a9efba
mb1921 <mb1921@student.aru.ac.uk>
2023-11-08 17:21:02 +0000

    Add project files.

---
+++ login_form/Form1.cs
@@ -0,0 +1,21 @@
+namespace login_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
