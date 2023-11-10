7041663a2f4fe45b770642032eede8245d84fc39
Rohit Panmand <panmandrohit7@gmail.com>
2023-11-10 12:33:51 +0000

    changed exit to return

--- login_form/Form1.Designer.cs
+++ login_form/Form1.Designer.cs
@@ -40,53 +40,55 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(430, 204);
+            label1.Location = new Point(389, 207);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(78, 20);
+            label1.Size = new Size(94, 25);
             label1.TabIndex = 0;
             label1.Text = "UserName";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(435, 291);
+            label2.Location = new Point(396, 304);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(70, 20);
+            label2.Size = new Size(87, 25);
             label2.TabIndex = 1;
             label2.Text = "Password";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(563, 193);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(544, 204);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(114, 27);
+            textBox1.Size = new Size(142, 31);
             textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(563, 280);
-            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Location = new Point(544, 304);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(114, 27);
+            textBox2.Size = new Size(142, 31);
             textBox2.TabIndex = 3;
             // 
             // button1
             // 
-            button1.Location = new Point(426, 377);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(314, 413);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(86, 31);
+            button1.Size = new Size(108, 39);
             button1.TabIndex = 4;
             button1.Text = "Enter";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(563, 377);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(487, 413);
+            button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
-            button2.Size = new Size(86, 31);
+            button2.Size = new Size(108, 39);
             button2.TabIndex = 5;
             button2.Text = "Reset";
             button2.UseVisualStyleBackColor = true;
@@ -94,20 +96,20 @@
             // 
             // button3
             // 
-            button3.Location = new Point(709, 377);
-            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Location = new Point(676, 413);
+            button3.Margin = new Padding(4, 5, 4, 5);
             button3.Name = "button3";
-            button3.Size = new Size(86, 31);
+            button3.Size = new Size(108, 39);
             button3.TabIndex = 6;
-            button3.Text = "Exit";
+            button3.Text = "return";
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(1142, 750);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -115,7 +117,7 @@
             Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 4, 3, 4);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
